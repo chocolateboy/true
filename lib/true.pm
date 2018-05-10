a22aa03d8df1de53d5c03dbb514f6365af736bcc
chocolateboy <chocolate@cpan.org>
2018-05-10 19:11:56 +0100

    add debug support:
    print debug messages to stderr if $TRUE_PM_DEBUG is set

--- lib/true.pm
+++ lib/true.pm
@@ -11,22 +11,31 @@ use XSLoader;
 our $VERSION = '0.18';
 our %TRUE;
 
+my $DEBUG = $ENV{TRUE_PM_DEBUG};
+
 XSLoader::load(__PACKAGE__, $VERSION);
 
-# XXX CopFILE(&PL_compiling) gives the wrong result here (it works in the OP checker in the XS).
-# return the full path of the file that's currently being compiled
+sub _debug ($) {
+    warn(@_, $/) if ($DEBUG);
+}
+
+# return the full path of the file that's currently being compiled.
+# XXX CopFILE(&PL_compiling) gives the wrong result here (it works in the
+# OP-checker in the XS).
 sub ccfile() {
     my ($file, $source, $line);
     my $trace = Devel::StackTrace->new;
 
     # find the innermost require frame
     #
-    # for "use Foo::Bar" or "require Foo::Bar", the evaltext contains "Foo/Bar.pm", and
-    # the filename/line refer to the file where the use/require statement appeared.
+    # for "use Foo::Bar" or "require Foo::Bar", the evaltext contains
+    # "Foo/Bar.pm", and the filename/line refer to the file where the
+    # use/require statement appeared.
 
     # work from the innermost frame out
     while (my $frame = $trace->next_frame) {
         next unless ($frame->is_require);
+
         my $required = $frame->evaltext;
 
         if (defined($file = $INC{$required})) {
@@ -42,13 +51,12 @@ sub ccfile() {
     return wantarray ? ($file, $source, $line) : $file;
 }
 
-# XXX should add a debug option
 sub import {
     my ($file, $source, $line) = ccfile();
 
     if (defined($file) && not($TRUE{$file})) {
         $TRUE{$file} = 1;
-        # warn "true: enabling true for $file at $source line $line", $/;
+        _debug "true: enabling true for $file at $source line $line";
         xs_enter();
     }
 }
@@ -57,7 +65,7 @@ sub unimport {
     my ($file, $source, $line) = ccfile();
 
     if (defined($file) && $TRUE{$file}) {
-        # warn "true: disabling true for $file at $source line $line", $/;
+        _debug "true: disabling true for $file at $source line $line";
         delete $TRUE{$file};
         xs_leave() unless (%TRUE);
     }
@@ -87,15 +95,15 @@ true - automatically return a true value when a file is required
 
 =head1 DESCRIPTION
 
-Perl's C<require> builtin (and its C<use> wrapper) requires the files it loads to return a true value.
-This is usually accomplished by placing a single
+Perl's C<require> builtin (and its C<use> wrapper) requires the files it loads
+to return a true value. This is usually accomplished by placing a single
 
     1;
 
-statement at the end of included scripts or modules. It's not onerous to add but it's
-a speed bump on the Perl novice's road to enlightenment. In addition, it appears to be
-a I<non-sequitur> to the uninitiated, leading some to attempt to mitigate its appearance
-with a comment:
+statement at the end of included scripts or modules. It's not onerous to add
+but it's a speed bump on the Perl novice's road to enlightenment. In addition,
+it appears to be a I<non-sequitur> to the uninitiated, leading some to attempt
+to mitigate its appearance with a comment:
 
     1; # keep require happy
 
@@ -107,23 +115,24 @@ or even:
 
     1; # Must end with this, because Perl is bogus.
 
-This module packages this "return true" behaviour so that it need not be written explicitly.
-It can be used directly, but it is intended to be invoked from the C<import> method of a
-L<Modern::Perl|Modern::Perl>-style module that enables modern Perl features and conveniences
-and cleans up legacy Perl warts.
+This module packages this "return true" behaviour so that it need not be
+written explicitly. It can be used directly, but it is intended to be invoked
+from the C<import> method of a L<Modern::Perl|Modern::Perl>-style module that
+enables modern Perl features and conveniences and cleans up legacy Perl warts.
 
 =head2 METHODS
 
 C<true> is file-scoped rather than lexically-scoped. Importing it anywhere in a
 file (e.g. at the top-level or in a nested scope) causes that file to return true,
-and unimporting it anywhere in a file restores the default behaviour. Redundant imports/unimports
-are ignored.
+and unimporting it anywhere in a file restores the default behaviour. Redundant
+imports/unimports are ignored.
 
 =head3 import
 
-Enable the "automatically return true" behaviour for the currently-compiling file. This should
-typically be invoked from the C<import> method of a module that loads C<true>. Code that uses
-this module solely on behalf of its callers can load C<true> without importing it e.g.
+Enable the "automatically return true" behaviour for the currently-compiling
+file. This should typically be invoked from the C<import> method of a module
+that loads C<true>. Code that uses this module solely on behalf of its callers
+can load C<true> without importing it e.g.
 
     use true (); # don't import
 
@@ -133,8 +142,8 @@ this module solely on behalf of its callers can load C<true> without importing i
 
     1;
 
-But there's nothing stopping a wrapper module also importing C<true> to obviate its own need to
-explicitly return a true value:
+But there's nothing stopping a wrapper module also importing C<true> to obviate
+its own need to explicitly return a true value:
 
     use true; # both load and import it
 
@@ -154,9 +163,10 @@ None by default.
 
 =head1 NOTES
 
-Because the unquoted name C<true> represents the boolean value C<true> in YAML, the module name must be
-quoted when written as a dependency in META.yml. In cases where this can't easily be done,
-a dependency can be declared on the package L<true::VERSION>, which has the same version as C<true.pm>.
+Because the unquoted name C<true> represents the boolean value C<true> in YAML,
+the module name must be quoted when written as a dependency in META.yml. In cases
+where this can't easily be done, a dependency can be declared on the package
+L<true::VERSION>, which has the same version as C<true.pm>.
 
 =head1 VERSION
 
